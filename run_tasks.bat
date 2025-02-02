7fc925beb897a002805f5fefaa64113b52aea52e
Anushka Srivastava <anushka22086@iiitd.ac.in>
2025-02-02 18:39:18 +0530

    final

---
+++ run_tasks.bat
@@ -0,0 +1,18 @@
+@echo off
+echo Running task1.py...
+cd task1
+python task1.py
+cd ..
+
+echo Running task2.py...
+cd task2
+python task2.py
+cd ..
+
+echo Running task3.py...
+cd task3
+python task3.py
+cd ..
+
+echo All tasks completed!
+pause
